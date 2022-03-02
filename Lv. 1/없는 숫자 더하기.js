e2a9a7f38aef278fe00bb0280077b1077a4a4833
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-03-02 15:20:29 +0900

    프로그래머스 Lv. 1
    프로그래머스 Lv. 1

---
+++ Lv. 1/없는 숫자 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    let sum = 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+    let sum_1 = 0
+    
+    
+    console.log(numbers);
+    
+    for (let i = 0; i < numbers.length; i++) {
+        sum_1 = sum_1 + numbers[i];
+    }
